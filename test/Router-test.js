4c50f6b644b64969b9f04ff0b03da13f8ba00c6f
Luis Pablo <luispablo.gallo@gmail.com>
2018-08-08 15:37:56 -0300

    Replace tabs with spaces

--- test/Router-test.js
+++ test/Router-test.js
@@ -9,24 +9,24 @@ var SECRET = "secret";
 var VALIDITY_DAYS = 7;
 
 var authenticatorMock = {
-	authenticate: function (username, password) {
-		return new Promise(function (resolve, reject) {
-			if (username === USERNAME && password === PASSWORD) resolve();
-			else reject({code: 401, message: "Invalid username or password"});
-		});
-	}
+  authenticate: function (username, password) {
+    return new Promise(function (resolve, reject) {
+      if (username === USERNAME && password === PASSWORD) resolve();
+      else reject({code: 401, message: "Invalid username or password"});
+    });
+  }
 };
 var mockLog = { debug: function (msg) { this.lastMessage = msg; } };
 var reqMock = {body: {}};
 
 var buildResMock = function () {
-	return {
-		status: function (code) { this.statusCode = code; return this; },
-		sendStatus: function (code) { this.status(code); },
-		send: function (message) { this.object = message; },
-		json: function (object) { this.object = object; },
-		end: function () {}
-	};
+  return {
+    status: function (code) { this.statusCode = code; return this; },
+    sendStatus: function (code) { this.status(code); },
+    send: function (message) { this.object = message; },
+    json: function (object) { this.object = object; },
+    end: function () {}
+  };
 };
 
 var resMock = buildResMock();
@@ -35,136 +35,136 @@ var validTokens = [];
 var routes = Router(validTokens)(authenticatorMock, SECRET, VALIDITY_DAYS, mockLog);
 
 test("Router - is logging", function (assert) {
-	reqMock.body = {};
+  reqMock.body = {};
 
-	assert.plan(1);
-	resMock.status = function () {
-		assert.equal(mockLog.lastMessage, "No username or password provided", "HTTP 422 message");
-		return this;
-	};
+  assert.plan(1);
+  resMock.status = function () {
+    assert.equal(mockLog.lastMessage, "No username or password provided", "HTTP 422 message");
+    return this;
+  };
 
-	routes.createToken()(reqMock, resMock);
+  routes.createToken()(reqMock, resMock);
 });
 
 test("Router - Create token", function (assert) {
-	reqMock.body.username = USERNAME;
-	reqMock.body.password = PASSWORD;
+  reqMock.body.username = USERNAME;
+  reqMock.body.password = PASSWORD;
 
-	assert.plan(2);
+  assert.plan(2);
 
-	resMock.json = function (object) {
-		assert.ok(object.token, "Exists the token property");
-		assert.equal(object.user.username, USERNAME, "Token built for the username provided");
-	};
+  resMock.json = function (object) {
+    assert.ok(object.token, "Exists the token property");
+    assert.equal(object.user.username, USERNAME, "Token built for the username provided");
+  };
 
-	routes.createToken()(reqMock, resMock);
+  routes.createToken()(reqMock, resMock);
 });
 
 test("Router - Additional data in token", function (assert) {
-	assert.plan(3);
-	var additionalData = { field1: "value1", field2: "value2" };
-	var additionalDataAuth = {
-		authenticate: function () {
-			return new Promise(function (resolve) { resolve(additionalData); });
-		}
-	};
-	resMock.json = function (object) {
-		assert.equal(object.user.username, "username", "The username is still there");
-		assert.equal(object.user.field1, additionalData.field1, "First additional data field");
-		assert.equal(object.user.field2, additionalData.field2, "Second additional data field");
-	};
-	var routesAdditionalData = Router(validTokens)(additionalDataAuth, SECRET, VALIDITY_DAYS, mockLog);
-	routesAdditionalData.createToken()(reqMock, resMock);
+  assert.plan(3);
+  var additionalData = { field1: "value1", field2: "value2" };
+  var additionalDataAuth = {
+    authenticate: function () {
+      return new Promise(function (resolve) { resolve(additionalData); });
+    }
+  };
+  resMock.json = function (object) {
+    assert.equal(object.user.username, "username", "The username is still there");
+    assert.equal(object.user.field1, additionalData.field1, "First additional data field");
+    assert.equal(object.user.field2, additionalData.field2, "Second additional data field");
+  };
+  var routesAdditionalData = Router(validTokens)(additionalDataAuth, SECRET, VALIDITY_DAYS, mockLog);
+  routesAdditionalData.createToken()(reqMock, resMock);
 });
 
 test("Router - Unauthorized create token", function (assert) {
-	assert.plan(1);
-	reqMock.body = { username: USERNAME, password: PASSWORD };
-	authenticatorMock.groups = function () { return new Promise(function (resolve) { resolve(["Group1", "TeamA"]); }); };
-	resMock.json = function () {};
-	resMock.status = function (code) { assert.equal(code, 403, "HTTP unauthorized"); return this; };
-	routes.createToken(["Group2", "TeamB"])(reqMock, resMock);
+  assert.plan(1);
+  reqMock.body = { username: USERNAME, password: PASSWORD };
+  authenticatorMock.groups = function () { return new Promise(function (resolve) { resolve(["Group1", "TeamA"]); }); };
+  resMock.json = function () {};
+  resMock.status = function (code) { assert.equal(code, 403, "HTTP unauthorized"); return this; };
+  routes.createToken(["Group2", "TeamB"])(reqMock, resMock);
 });
 
 test("Router - Authorized create token", function (assert) {
-	assert.plan(1);
-	reqMock.body = { username: USERNAME, password: PASSWORD };
-	authenticatorMock.groups = function () { return new Promise(function (resolve) { resolve(["Group1", "TeamA"]); }); };
-	resMock.json = function (object) { assert.ok(object.token, "Exists the token property"); };
-	routes.createToken(["Group2", "TeamA"])(reqMock, resMock);
+  assert.plan(1);
+  reqMock.body = { username: USERNAME, password: PASSWORD };
+  authenticatorMock.groups = function () { return new Promise(function (resolve) { resolve(["Group1", "TeamA"]); }); };
+  resMock.json = function (object) { assert.ok(object.token, "Exists the token property"); };
+  routes.createToken(["Group2", "TeamA"])(reqMock, resMock);
 });
 
 test("Router - No username or no password", function (assert) {
-	reqMock.body = {};
+  reqMock.body = {};
 
-	assert.plan(1);
-	resMock.status = function (code) {
-		assert.equal(code, 422, "HTTP error 422 because of missing credentials");
-		return this;
-	};
+  assert.plan(1);
+  resMock.status = function (code) {
+    assert.equal(code, 422, "HTTP error 422 because of missing credentials");
+    return this;
+  };
 
-	routes.createToken()(reqMock, resMock);
+  routes.createToken()(reqMock, resMock);
 });
 
 test("Router - Invalid username or password", function (assert) {
   resMock = buildResMock();
-	reqMock.body.username = "invalid";
-	reqMock.body.password = "invalid";
+  reqMock.body.username = "invalid";
+  reqMock.body.password = "invalid";
 
-	assert.plan(1);
+  assert.plan(1);
 
-	resMock.status = function (code) {
-		assert.equal(code, 401, "HTTP error 401: unauthorized");
+  resMock.status = function (code) {
+    assert.equal(code, 401, "HTTP error 401: unauthorized");
     return this;
-	};
+  };
 
-	routes.createToken()(reqMock, resMock);
+  routes.createToken()(reqMock, resMock);
 });
 
 test("Router - Validate existing & valid token", function (assert) {
-	reqMock.body = {username: USERNAME, password: PASSWORD};
+  reqMock.body = {username: USERNAME, password: PASSWORD};
 
-	assert.plan(1);
+  assert.plan(1);
 
-	resMock.json = function (token) {
-		reqMock.headers = {"x-access-token": token.token};
-		resMock.status = function (code) {
-			assert.equal(code, 200, "HTTP OK");
-			return this;
-		};
-		routes.validateToken(reqMock, resMock);
-	};
+  resMock.json = function (token) {
+    reqMock.headers = {"x-access-token": token.token};
+    resMock.status = function (code) {
+      assert.equal(code, 200, "HTTP OK");
+      return this;
+    };
+    routes.validateToken(reqMock, resMock);
+  };
 
-	routes.createToken()(reqMock, resMock);
+  routes.createToken()(reqMock, resMock);
 });
 
 test("Router - Validate non-existent token", function (assert) {
-	assert.plan(1);
+  assert.plan(1);
 
-	reqMock.headers = {"x-access-token": "nonexistent"};
-	resMock.status = function (code) {
-		assert.equal(code, 401, "HTTP unauthorized");
-		return this;
-	};
-	routes.validateToken(reqMock, resMock);
+  reqMock.headers = {"x-access-token": "nonexistent"};
+  resMock.status = function (code) {
+    assert.equal(code, 401, "HTTP unauthorized");
+    return this;
+  };
+  routes.validateToken(reqMock, resMock);
 });
 
 test("Router - Validate expired token", function (assert) {
-	var expiredRoutes = Router(validTokens)(authenticatorMock, SECRET, -1, mockLog); // negative days for generating expired tokens
-	reqMock.body = {username: USERNAME, password: PASSWORD};
+  var expiredRoutes = Router(validTokens)(authenticatorMock, SECRET, -1, mockLog); // negative days for generating expired tokens
+  reqMock.body = {username: USERNAME, password: PASSWORD};
 
-	assert.plan(1);
+  assert.plan(1);
 
-	resMock.json = function (token) {
-		reqMock.headers = {"x-access-token": token.token};
-		resMock.status = function (code) {
-			assert.equal(code, 401, "HTTP unauthorized, expired");
-			return this;
-		};
-		expiredRoutes.validateToken(reqMock, resMock);
-	};
+  resMock.json = function (token) {
+    reqMock.headers = {"x-access-token": token.token};
+    resMock.status = function (code) {
+      assert.equal(code, 401, "HTTP unauthorized, expired");
+      return this;
+    };
+    expiredRoutes.validateToken(reqMock, resMock);
+  };
 
-	expiredRoutes.createToken()(reqMock, resMock);
+  expiredRoutes.createToken()(reqMock, resMock);
 });
 
 test("Router - validateCredentials - correct username and password in HTTP header", function (assert) {
@@ -203,23 +203,23 @@ test("Router - validateCredentials - incorrect password in HTTP header", functio
 });
 
 test("Router - delete token", function (assert) {
-	reqMock.body.username = USERNAME;
-	reqMock.body.password = PASSWORD;
+  reqMock.body.username = USERNAME;
+  reqMock.body.password = PASSWORD;
 
-	var deleteResMock = buildResMock();
+  var deleteResMock = buildResMock();
 
-	assert.plan(1);
+  assert.plan(1);
 
-	deleteResMock.json = function (token) {
-		reqMock.headers = {"x-access-token": token};
-		deleteResMock.end = function () {
-			deleteResMock.status = function (code) {
-				assert.equal(code, 401, "No longer valid");
-				return this;
-			};
-			routes.validateToken(reqMock, deleteResMock);
-		};
-		routes.deleteToken(reqMock, deleteResMock);
-	};
-	routes.createToken()(reqMock, deleteResMock);
+  deleteResMock.json = function (token) {
+    reqMock.headers = {"x-access-token": token};
+    deleteResMock.end = function () {
+      deleteResMock.status = function (code) {
+        assert.equal(code, 401, "No longer valid");
+        return this;
+      };
+      routes.validateToken(reqMock, deleteResMock);
+    };
+    routes.deleteToken(reqMock, deleteResMock);
+  };
+  routes.createToken()(reqMock, deleteResMock);
 });
