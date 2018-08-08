7da43a5de135ea8315b4cb2947f4a3a69b6d3308
Luis Pablo <luispablo.gallo@gmail.com>
2018-08-08 15:38:20 -0300

    Replace tabs with spaces

--- test/index-test.js
+++ test/index-test.js
@@ -3,7 +3,7 @@ var index = require("../index");
 var AuthFetch = require("../lib/AuthFetch");
 
 test("index - exports", function (assert) {
-	assert.ok(index({}) !== null, "Something exported as default");
-	assert.equal(index.AuthFetch, AuthFetch, "exports AuthFetch without invoking function");
-	assert.end();
+  assert.ok(index({}) !== null, "Something exported as default");
+  assert.equal(index.AuthFetch, AuthFetch, "exports AuthFetch without invoking function");
+  assert.end();
 });
