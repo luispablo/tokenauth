490be02e11d3f112df3ac9fee2ce07c3749f14fc
Luis Pablo Gallo <luispablo.gallo@gmail.com>
2021-05-14 15:28:41 -0300

    Use new tokensManager in UserCheck

--- test/UserCheck-test.js
+++ test/UserCheck-test.js
@@ -1,77 +1,86 @@
-var test = require("ava");
-var jwt = require("jwt-simple");
-var UserCheck = require("../lib/UserCheck");
+
 const addDays = require("date-fns/add_days");
+const jwt = require("jwt-simple");
 const subDays = require("date-fns/sub_days");
+const test = require("ava");
+const tokensManager = require("../lib/tokensManager");
+const UserCheck = require("../lib/UserCheck");
 
-var SECRET = "secret";
-var USERNAME_1 = "username1";
-var USERNAME_2 = "username2";
+const SECRET = "secret";
+const USERNAME_1 = "username1";
+const USERNAME_2 = "username2";
 
-var exp = addDays(new Date(), 7).getTime() / 1000; // 7 days from today
-var DECODED_TOKEN_1 = {iss: USERNAME_1, exp };
-var TOKEN_1 = jwt.encode(DECODED_TOKEN_1, SECRET);
-var TOKEN_2 = jwt.encode({iss: USERNAME_2, exp }, SECRET);
-var INVALID_TOKEN = "invalid_token";
+const exp = addDays(new Date(), 7).getTime() / 1000; // 7 days from today
+const DECODED_TOKEN_1 = {iss: USERNAME_1, exp };
+const TOKEN_1 = jwt.encode(DECODED_TOKEN_1, SECRET);
+const TOKEN_2 = jwt.encode({iss: USERNAME_2, exp }, SECRET);
+const INVALID_TOKEN = "invalid_token";
 
-var mockLog = { debug: function (msg) { this.lastMessge = msg; } };
-var check = UserCheck([TOKEN_1, TOKEN_2], SECRET, mockLog);
+const mockLog = { debug: function (msg) { this.lastMessge = msg; } };
+const tokens = tokensManager();
+tokens.add(TOKEN_1);
+tokens.add(TOKEN_2);
+const check = UserCheck(tokens, SECRET, mockLog);
 
-test.cb("UserCheck - is logging", function (assert) {
+test.cb("UserCheck - is logging", function (t) {
   check(INVALID_TOKEN).catch(function () {
-    assert.is(mockLog.lastMessge, "Invalid token", "Logged debug message");
-    assert.end();
+    t.is(mockLog.lastMessge, "Invalid token", "Logged debug message");
+    t.end();
   });
 });
 
-test.serial("UserCheck - invalid token", async function (assert) {
-  assert.plan(1);
+test.serial("UserCheck - invalid token", async function (t) {
+  t.plan(1);
 
   check(INVALID_TOKEN).then(function () {
-    assert.fail("Should have failed");
+    t.fail("Should have failed");
   }).catch(function (error) {
-    assert.is(error.message, "Invalid token", "Invalid token message");
+    t.is(error.message, "Invalid token", "Invalid token message");
   });
 });
 
-test.serial("UserCheck - expired token", async function (assert) {
-  assert.plan(1);
+test.serial("UserCheck - expired token", async function (t) {
+  t.plan(1);
 
   const yesterdayInSeconds = subDays(new Date(), 1).getTime() / 1000;
-  var expiredToken = jwt.encode({ sub: USERNAME_1, exp: yesterdayInSeconds }, SECRET);
-  var expiredCheck = UserCheck([expiredToken], SECRET, mockLog);
+  const expiredToken = jwt.encode({ sub: USERNAME_1, exp: yesterdayInSeconds }, SECRET);
+  const tokens1 = tokensManager();
+  tokens1.add(expiredToken)
+  const expiredCheck = UserCheck(tokens1, SECRET, mockLog);
 
   expiredCheck(expiredToken).then(function () {
-    assert.fail("Should have failed");
+    t.fail("Should have failed");
   }).catch(function (error) {
-    assert.is(error.message, "Token expired", "Token expired message");
+    t.is(error.message, "Token expired", "Token expired message");
   })
 });
 
-test.serial("UserCheck - no valid tokens", async function (assert) {
-  assert.plan(1);
-  var emptyCheck = UserCheck([], SECRET, mockLog);
+test.serial("UserCheck - no valid tokens", async function (t) {
+  t.plan(1);
+  const tokens1 = tokensManager();
+  tokens1.clear();
+  const emptyCheck = UserCheck(tokens1, SECRET, mockLog);
 
   emptyCheck(TOKEN_1).then(function () {
-    assert.fail("Should have failed");
+    t.fail("Should have failed");
   }).catch(function (error) {
-    assert.is(error.message, "Invalid token", "Invalid token message");
+    t.is(error.message, "Invalid token", "Invalid token message");
   });
 });
 
-test.serial("UserCheck - valid token", async function (assert) {
-  assert.plan(1);
+test.serial("UserCheck - valid token", async function (t) {
+  t.plan(1);
 
   check(TOKEN_1).then(function () {
-    assert.pass("Token valid");
+    t.pass("Token valid");
   }).catch(function (error) {
-    assert.fail(error.message);
+    t.fail(error.message);
   });
 });
 
-test.cb("UserCheck - resolve with decoded token", function (assert) {
+test.cb("UserCheck - resolve with decoded token", function (t) {
   check(TOKEN_1).then(function (decodedToken) {
-    assert.deepEqual(decodedToken, DECODED_TOKEN_1);
-    assert.end();
+    t.deepEqual(decodedToken, DECODED_TOKEN_1);
+    t.end();
   });
 });
